677e5df1640881215fb5b31c0e90b4b5f9052a98
Mario Lazzari <mario.lazzari@gmail.com>
2025-01-04 22:50:10 +0100

    Done lesson 76

--- types/index.ts
+++ types/index.ts
@@ -2,8 +2,10 @@ import { z } from "zod";
 import {
   cartItemSchema,
   insertCartSchema,
+  insertOrderSchema,
   insertProductSchema,
   paymentMethodSchema,
+  paymentResultSchema,
   shippingAddressSchema,
 } from "@/lib/validators";
 import { PropsWithChildren } from "react";
@@ -38,3 +40,21 @@ export type CartItem = z.infer<typeof cartItemSchema>;
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
 
 export type PaymentMethod = z.infer<typeof paymentMethodSchema>;
+
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string;
+  createdAt: Date;
+  isPaid: Boolean;
+  paidAt: Date | null;
+  isDelivered: Boolean;
+  deliveredAt: Date | null;
+  orderitems: OrderItem[];
+  user: {
+    name: string;
+    email: string;
+  };
+  paymentResult: PaymentResult;
+};
+export type OrderItem = z.infer<typeof cartItemSchema>;
+
+export type PaymentResult = z.infer<typeof paymentResultSchema>;
