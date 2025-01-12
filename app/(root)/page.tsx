f9e58553d6a36e6c9a9ff8067c345bdb69844dc7
Mario Lazzari <mario.lazzari@gmail.com>
2025-01-12 10:02:40 +0100

    Done lesson 126

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,17 @@
-import { getLatestProducts } from "@/lib/actions/product";
+import { getFeaturedProducts, getLatestProducts } from "@/lib/actions/product";
 import ProductList from "@/components/shared/product/product-list";
 import { Product } from "@/types";
+import ProductCarousel from "@/components/shared/product/product-carousel";
 
 async function HomePage() {
   const lastProds: Product[] = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
   return (
     <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList title="New Arrivals" data={lastProds} limit={4} />
     </>
   );
